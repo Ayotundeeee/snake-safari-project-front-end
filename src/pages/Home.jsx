740c3c8e2481ba08bc3d1d7134df19eb18e7af08
AAkinkunmi <aakinkunmi@pursuit.org>
2024-07-23 10:46:31 -0400

    adjust form styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ import '../styles/home.css'
 const Home = () => {
     return (
         <div className='home-page'>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRijXANAwVLOeb9s_wHGuXK1njH7nZ_16ATSA&s" alt="" />
-            <p>Welcome To The Snake Safari!! 
-            <br />Let's Explore.
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU3xCF8qjqfbiT2hMa7iuQ93QF5W2n6Adszg&s" alt="" />
+            <p>Welcome To Your Snake Safari!! 
+            <br />
             Click on Snake Collection to view your collection.</p>
         </div>
     );
